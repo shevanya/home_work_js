b791797b73771007acfe8bc3b8781cabfe4930fa
Shevchuk Dmitry <shevanyako@gmail.com>
2018-03-11 15:00:42 +0200

    ome_work_ready

---
+++ js/main.js
@@ -0,0 +1,96 @@
+var myNumber = 25;
+    myString = 'string #1';
+    myBool = true;
+    myNull = null;
+    myUnder = undefined; 
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBool);
+console.log(myNull);
+console.log(myUnder);
+
+
+console.log(myNumber.toPrecision(4));
+
+
+var i = 10;
+console.log(i);
+
+console.log(+i);
+console.log(i);
+
+console.log(i++);
+console.log(i);
+
+console.log(-i);
+console.log(i);
+
+console.log(i--);
+console.log(i);
+
+var x = 50;
+console.log(x);
+
+x += 20;
+console.log(x);
+
+x -= 30;
+console.log(x);
+
+x *= 4;
+console.log(x);
+
+x /= 8;
+console.log(x);
+
+x %= 50;
+console.log(x);
+
+console.log(x === 49);
+console.log(x > 0);
+console.log(x < 200);
+
+console.log('string #1'.length);
+
+var myString = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+console.log( myString )
+
+console.log(myString.indexOf('sit'));
+console.log(myString.indexOf('Lor'));
+console.log(myString.indexOf('met'));
+
+console.log(myString.replace('sit','456'));
+console.log(myString.replace('Lor','123'));
+console.log(myString.replace('sit','789'));
+
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(Math.PI); 
+console.log(Math.round (89.279))  
+console.log(Math.round ((Math.random ()*10))); 
+console.log(Math.pow(2,5));
+
